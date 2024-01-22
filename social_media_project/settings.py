74569bf0b6e88ab4eabcef6827f0f87cf4139c1a
sanjueranga <sanjueranga16@gmail.com>
2024-01-22 13:02:34 +0530

    models ser

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-88gu406!!d2+)#l@4y+#b@4*h&-a2f4)*v%#%3=9ec#!r2ws9g'
+SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
